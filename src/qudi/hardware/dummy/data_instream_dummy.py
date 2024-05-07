88d788153dc0272a9795b60be7f4727bfd49bce8
MK <manuel.kuhrke@uni-ulm.de>
2024-05-07 16:41:16 +0200

    made noise more random

--- src/qudi/hardware/dummy/data_instream_dummy.py
+++ src/qudi/hardware/dummy/data_instream_dummy.py
@@ -20,7 +20,7 @@ See the GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along with qudi.
 If not, see <https://www.gnu.org/licenses/>.
 """
-
+"Manuel war hier"
 import time
 import numpy as np
 from enum import Enum
@@ -37,7 +37,7 @@ from qudi.interface.data_instream_interface import StreamingMode, SampleTiming
 def _make_sine_func(sample_rate: float) -> Callable[[np.ndarray, np.ndarray], None]:
     freq = sample_rate / (20 + 80 * np.random.rand())
     amp = 1 + np.random.rand() * 9
-    noise_lvl = amp * (0.1 + np.random.rand() * 0.4)
+    noise_lvl = amp * (0.1 + np.random.rand() * np.random.rand())
     def make_sine(x, y):
         # y[:] = np.sin(2 * np.pi * freq * x)
         np.sin(2 * np.pi * freq * x, out=y)
